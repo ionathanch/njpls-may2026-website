818c1baf0dc408025f57b37c098cd421699efec1
Jonathan Chan <jonathanhwchan@gmail.com>
2026-02-02 18:20:47 -0500

    Update for May 2026

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   // Watch CSS files for changes
-  eleventyConfig.addPassthroughCopy({ "assets": "may2023" })
+  eleventyConfig.addPassthroughCopy({ "assets": "may2026" })
   eleventyConfig.setBrowserSyncConfig({
     files: './_site/**/*.css'
   });
